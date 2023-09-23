10f453fe9f189930278582fd5f1ad5983242f4ea
Philipp <pmeising@debian.local>
2023-09-23 14:36:01 +0100

    .

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,38 +1,44 @@
 #!/bin/sh
 
+# ANSI escape codes for text colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'  # Reset text color to default
+
 if [ ! -d /run/myysqld ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
 fi
 
 if [ -d /var/lib/mysql/mysql ]; then
-	echo 'MySQL already initialized'
-	echo 'Skipping configuration...'
+    echo -e "${GREEN}MySQL already initialized${RESET}"
+    echo -e "${GREEN}Skipping configuration...${RESET}"
 else
-	echo 'Initializing MySQL...'
-	mkdir -p /var/lib/mysql
-	chown -R mysql:mysql /var/lib/mysql
+    echo -e "${YELLOW}Initializing MySQL...${RESET}"
+    mkdir -p /var/lib/mysql
+    chown -R mysql:mysql /var/lib/mysql
 
-	# Perform database initialisation
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql &> /dev/null
-	echo "Initialization successful"
+    # Perform database initialization
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql &> /dev/null
+    echo -e "${GREEN}Initialization successful${RESET}"
 fi
 
-# Check if database already exist. Set it up if not.
+# Check if database already exists. Set it up if not.
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-	echo "MySQL Database: $MYSQL_DATABASE already set up"
-	echo "Skipping setup..."
+    echo -e "${GREEN}MySQL Database: $MYSQL_DATABASE already set up${RESET}"
+    echo -e "${GREEN}Skipping setup...${RESET}"
 else
-	echo "Setting up MySQL database: $MYSQL_DATABASE"
-	echo "Changing MySQL root password to: $MYSQL_ROOT_PASSWORD"
+    echo -e "${YELLOW}Setting up MySQL database: $MYSQL_DATABASE${RESET}"
+    echo -e "${YELLOW}Changing MySQL root password to: $MYSQL_ROOT_PASSWORD${RESET}"
 
-	tempfile=`mktemp`
-	if [ ! -f "$tempfile" ]; then
-		return 1
-	fi
+    tempfile=$(mktemp)
+    if [ ! -f "$tempfile" ]; then
+        return 1
+    fi
 
-	echo "Creating tempfile: $tempfile"
-	cat << EOF > $tempfile
+    echo -e "${YELLOW}Creating tempfile: $tempfile${RESET}"
+    cat <<EOF > "$tempfile"
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
@@ -42,23 +48,23 @@ DROP DATABASE IF EXISTS test;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
 EOF
 
-	echo "Creating database: $MYSQL_DATABASE"
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> $tempfile
+    echo -e "${YELLOW}Creating database: $MYSQL_DATABASE${RESET}"
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> "$tempfile"
 
-	echo "Creating user: $MYSQL_USER with password: $MYSQL_PASSWORD"
-	echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tempfile
+    echo -e "${YELLOW}Creating user: $MYSQL_USER with password: $MYSQL_PASSWORD${RESET}"
+    echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$tempfile"
 
-	echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> $tempfile
+    echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$tempfile"
 
-	echo "FLUSH PRIVILEGES;" >> $tempfile
+    echo "FLUSH PRIVILEGES;" >> "$tempfile"
 
-	echo "Applying changes by running tempfile: $tempfile in bootstrap mode"
-	/usr/bin/mysqld --user=mysql --bootstrap < $tempfile
-	rm -f $tempfile
+    echo -e "${YELLOW}Applying changes by running tempfile: $tempfile in bootstrap mode${RESET}"
+    /usr/bin/mysqld --user=mysql --bootstrap < "$tempfile"
+    rm -f "$tempfile"
 fi
 
 sed -i "s/skip-networking/#skip-networking/g" /etc/my.cnf.d/mariadb-server.cnf
-echo "[mysqld]\nbind-address=0.0.0.0" >> /etc/my.cnf.d/mariadb-server.cnf
+echo -e "[mysqld]\nbind-address=0.0.0.0" >> /etc/my.cnf.d/mariadb-server.cnf
 
-echo 'Starting /usr/bin/mysqld process'
+echo -e "${GREEN}Starting /usr/bin/mysqld process${RESET}"
 exec "$@" &> /dev/null
--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -1,37 +1,42 @@
 #!/bin/sh
 
-# variables
+# Variables
 FILE_PATH_CONFIG_SRC="./wordpress/wp-config-sample.php"
 FILE_PATH_CONFIG_DST="./wordpress/wp-config.php"
 
+# ANSI escape codes for text colors
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'  # Reset text color to default
+
 # -f -> does it exist?
 if [ -f ./wordpress/wp-config-sample.php ]; then
-	echo 'Wordpress already installed'
-	echo 'Skipping download...'
+    echo -e "${GREEN}WordPress already installed${RESET}"
+    echo -e "${GREEN}Skipping download...${RESET}"
 else
-	echo 'Downloading Wordpress...'
-	wget https://wordpress.org/latest.tar.gz
-	tar -xzvf /var/www/html/latest.tar.gz
-	rm -f latest.tar.gz
-	chown -R www-data:www-data wordpress
-	echo 'Download successful'
+    echo -e "${YELLOW}Downloading WordPress...${RESET}"
+    wget https://wordpress.org/latest.tar.gz
+    tar -xzvf /var/www/html/latest.tar.gz
+    rm -f latest.tar.gz
+    chown -R www-data:www-data wordpress
+    echo -e "${GREEN}Download successful${RESET}"
 fi
 
-# wp-config.php only exists if wordpress was configured.
+# wp-config.php only exists if WordPress was configured.
 if [ -f $FILE_PATH_CONFIG_DST ]; then
-	echo 'Wordpress already initialized'
-	echo 'Skipping configuration...'
+    echo -e "${GREEN}WordPress already initialized${RESET}"
+    echo -e "${GREEN}Skipping configuration...${RESET}"
 else
-	echo 'Creating wp-config.php...'
-	# sed -> Stream editor -i (in place change -> effectively overwriting) s/ -> search. /g (greedy) -> takes all of them
-	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g"  $FILE_PATH_CONFIG_SRC
-	sed -i "s/username_here/$WORDPRESS_DB_USER/g"           $FILE_PATH_CONFIG_SRC
-	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g"  $FILE_PATH_CONFIG_SRC
-	sed -i "s/localhost/$WORDPRESS_DB_HOST/g"          $FILE_PATH_CONFIG_SRC
-	mv $FILE_PATH_CONFIG_SRC $FILE_PATH_CONFIG_DST
+    echo -e "${YELLOW}Creating wp-config.php...${RESET}"
+    # sed -> Stream editor -i (in place change -> effectively overwriting) s/ -> search
+    sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g"  $FILE_PATH_CONFIG_SRC
+    sed -i "s/username_here/$WORDPRESS_DB_USER/g"           $FILE_PATH_CONFIG_SRC
+    sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g"  $FILE_PATH_CONFIG_SRC
+    sed -i "s/localhost/$WORDPRESS_DB_HOST/g"          $FILE_PATH_CONFIG_SRC
+    mv $FILE_PATH_CONFIG_SRC $FILE_PATH_CONFIG_DST
 fi
 
-echo 'Starting /usr/sbin/php-fpm8 process in foreground'
+echo -e "${GREEN}Starting /usr/sbin/php-fpm8 process in foreground${RESET}"
 
 exec "$@"
 
